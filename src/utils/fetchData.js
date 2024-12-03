b6cf84754717a054406055f7791d44d98dcbbbac
cierra <138401596+cdcutshaw@users.noreply.github.com>
2024-12-02 17:21:27 -0700

    fix funcionality to fetch data from API

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,32 +1,49 @@
-const API_KEY = 'adf44b533401417c125f804a142fedec';
-const SHOW_ID = 94605;
-const displayedCharacters = ['Jinx', 'Vi']
+const API_KEY = 'CYz6vjAnJF12HerHurgbirG3MybbJp3h';
+
+const displayedCharacters = [
+    {displayName: 'Jinx',  gifId: 'PM7EBVwNHRuXYKIsJf'},
+    {displayName: 'Vi',  gifId: 'ULd32LlfkRFUQEZVi5'},
+    {displayName: 'Vander', gifId: '1Ukza4n3t9xC7EJzPI'},
+    {displayName: 'Viktor', gifId: 'ue91KrnPxbQNyM8bay'},
+    {displayName: 'Jayce', gifId: 'dfdo2x7akl3UPrb9xC'},
+    {displayName: 'Mel', gifId: 'moMT9Qrn5PDq3VxsSs'},
+    {displayName: 'Heimerdinger', gifId: 'kZOxW6jOTFqVTFtYf1'},
+    {displayName: 'Silco', gifId: '5JPRXRZnH1JlvdtZMs'},
+    {displayName: 'Ekko', gifId: 'WXFLTuHi6F2E8sll5S'},
+    {displayName: 'Caitlyn', gifId: '7F6oMII4D1RL9GWupT'},
+    {displayName: 'Singed', gifId: 'sObkolEu4ak4RLhvQE'},
+    {displayName: 'Sevika', gifId: 'G7nIVSQG3UriYvAKAM'},
+    
+];
 
 export default async function fetchData() {
-  const url = `https://api.themoviedb.org/3/tv/${SHOW_ID}/credits?api_key=${API_KEY}`;
-  
   try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Failed to fetch data: ${response.statusText}`);
+    const characters = await Promise.all(
+      displayedCharacters.map(async ({displayName, gifId}) => {
+        const url = `https://api.giphy.com/v1/gifs/${gifId}?api_key=${API_KEY}`;
+
+
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch GIF for ${displayName}: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        const gifImage = data.data.images.fixed_width.url;
+
+        return {
+            id: gifId, // You can keep track of the GIF id
+            name: displayName,
+            image: gifImage,
+            isClicked: false,
+          };
+        })
+      );
+  
+      return characters;
+    } catch (error) {
+      console.error('Error fetching data from Giphy:', error);
+      return [];
     }
-
-    const data = await response.json();
-    console.log(data);
-
-    // Extract relevant character data
-    const characters = data.cast
-      .filter((character) => displayedCharacters.includes(character.name)) // Filter by name
-      .map((character) => ({
-        id: character.id,
-        name: character.name,
-        image: `https://image.tmdb.org/t/p/w500${character.profile_path}`, // Construct image URL
-        isClicked: false,
-      }));
-
-    return characters;
-  } catch (error) {
-    console.error('Error fetching characters:', error);
-    return [];
-  }
-}
+  }  
